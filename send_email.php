e141838d83eed61289345eab1ac8911872de7f9c
Abdullah Mohammad Jaid <152844575+AbdullahMohammadJaid@users.noreply.github.com>
2025-11-05 12:14:21 +0800

    Add send_email.php for processing contact form submissions

---
+++ send_email.php
@@ -0,0 +1,45 @@
+<?php
+header('Content-Type: application/json');
+
+// Only process POST requests
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the form fields and sanitize them
+    $name = strip_tags(trim($_POST["name"]));
+    $name = str_replace(array("\r","\n"),array(" "," "),$name);
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $message = trim($_POST["message"]);
+
+    // Check that data was sent to the mailer
+    if (empty($name) || empty($message) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        http_response_code(400);
+        echo json_encode(["message" => "Please complete the form and try again."]);
+        exit;
+    }
+
+    // Set the recipient email address
+    $recipient = "_mainaccount@amjaid.com";
+    
+    // Set the email subject
+    $subject = "New contact from $name";
+    
+    // Build the email content
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
+    
+    // Build the email headers
+    $email_headers = "From: $name <$email>";
+    
+    // Send the email
+    if (mail($recipient, $subject, $email_content, $email_headers)) {
+        http_response_code(200);
+        echo json_encode(["message" => "Thank you! Your message has been sent."]);
+    } else {
+        http_response_code(500);
+        echo json_encode(["message" => "Oops! Something went wrong and we couldn't send your message."]);
+    }
+} else {
+    http_response_code(403);
+    echo json_encode(["message" => "There was a problem with your submission, please try again."]);
+}
+?>
